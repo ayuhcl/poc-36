9f9fa6dd1845bfcd72e8c54c767aa4ec32242a42
ayuhcl <ayushi-agarwal@hcltech.com>
2026-05-21 18:07:46 +0530

    Delete app.js

--- app.js
+++
@@ -1,9 +0,0 @@
-module.exports = async function (context, req) {
-    context.log('HTTP trigger function processed a request.');
-
-    const name = req.query.name || "Guest";
-
-    context.res = {
-        body: "Hello " + name
-    };
-};
